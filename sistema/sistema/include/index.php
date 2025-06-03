07e97a9d11aa5ad28840ebbbc9ae9e780ea65da0
FraNFraN <134552777+BatataDeCodigos@users.noreply.github.com>
2025-06-03 20:06:13 -0300

    papapa

---
+++ sistema/sistema/include/index.php
@@ -0,0 +1,21 @@
+<?php 
+// include dos arquivox
+include_once './include/logado.php';
+include_once './include/conexao.php';
+include_once './include/header.php';
+?>
+
+  
+  <main>
+
+<h1>
+
+    Olá mundo!
+
+</h1>
+   
+  </main>
+  <?php 
+  // include dos arquivox
+  include_once './include/footer.php';
+  ?>
